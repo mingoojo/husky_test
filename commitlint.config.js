b54a5937ca3a1d63423658a05b0f651a567a796a
mingoojo <swyks112@gmail.com>
2026-02-23 10:56:13 +0900

    test: commitlint추가

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
+
+// feat: 새 기능
+// fix: 버그 수정
+// docs: 문서 변경
+// style: 포맷팅 (코드 동작 변화 없음)
+// refactor: 리팩토링
+// test: 테스트 추가/수정
+// chore: 빌드, 설정 등 기타
+// perf: 성능 개선
+// ci: CI 설정 변경
+// build: 빌드 시스템 변경
+// revert: 이전 커밋 되돌리기
